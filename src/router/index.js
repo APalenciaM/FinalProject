e22f9aed50848227db5e47147e9d6d6d5efaccf2
Adrià <adria.palencia@tekmaneducation.com>
2023-05-12 11:38:02 +0200

    susbtring *

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/*',
+      path: '/',
       name: 'home',
       component: Login
     },
