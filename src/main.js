c1b22c8c16ac9a9f23bc0758e48088819b2ee9c1
Adrià <adria.palencia@tekmaneducation.com>
2023-05-12 11:36:58 +0200

    change name

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 
 import App from './App.vue'
-import router from './router/router.js'
+import router from './router/index.js'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
