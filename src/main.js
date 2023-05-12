503dcd755fc9da1bdaf7f2ada0090b6a4f7d9e9b
Adrià <adria.palencia@tekmaneducation.com>
2023-05-12 12:36:10 +0200

    index

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
-import { createPinia } from '../node_modules/pinia'
-import { createApp } from '../node_modules/vue'
+import { createPinia } from '../node_modules/pinia/index'
+import { createApp } from '../node_modules/vue/index'
 
 import App from './App.vue'
 import router from './router/index.js'
-import '../node_modules/bootstrap/dist/css/bootstrap.css'
-import '../node_modules/bootstrap-vue/dist/bootstrap-vue.css'
 
 import './assets/main.css'
 
