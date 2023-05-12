954b8a57abf90fe4c6525fd7d32abece7a9cbf11
Adrià <adria.palencia@tekmaneducation.com>
2023-05-12 12:52:27 +0200

    done

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
-import { createPinia } from './pinia.js'
-import { createApp } from './vue.js'
+import { createPinia } from 'pinia'
+import { createApp } from 'vue'
 
 import App from './App.vue'
 import router from './router/index.js'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 import './assets/main.css'
 
