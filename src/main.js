1f11f1022743ab62919fa8389dfff68a895fd8ac
Adrià <adria.palencia@tekmaneducation.com>
2023-05-12 11:28:27 +0200

    add route

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
-import { createPinia } from '@/pinia'
-import { createApp } from '@/vue'
+import { createPinia } from 'pinia'
+import { createApp } from 'vue'
 
 import App from './App.vue'
 import router from './router/router.js'
-import '@/bootstrap/dist/css/bootstrap.css'
-import '@/bootstrap-vue/dist/bootstrap-vue.css'
-
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 import './assets/main.css'
 
