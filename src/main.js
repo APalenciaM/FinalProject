a2b24420f04a0179320ced51755e8bf5a565b7d4
Adrià <adria.palencia@tekmaneducation.com>
2023-05-12 11:17:16 +0200

    add lasts paths

--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ import { createApp } from '/vue'
 
 import App from './App.vue'
 import router from './router/router.js'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import '/bootstrap/dist/css/bootstrap.css'
+import '/bootstrap-vue/dist/bootstrap-vue.css'
 
 
 import './assets/main.css'
